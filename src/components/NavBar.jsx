85f01f0f17385c20af32b907054e94f479b8ace0
German <trianogerman@gmail.com>
2024-05-30 21:04:49 -0300

    fixNavBarDesplegable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,6 +11,9 @@ function NavBar() {
                     <Link to={"/"} className="navbar-brand">
                         <img src="https://www.pani.com.ar/cdn/shop/files/Pani-Banderita-2_140x.png?v=1613699994" alt="logo-pani" />
                     </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item dropdown">
